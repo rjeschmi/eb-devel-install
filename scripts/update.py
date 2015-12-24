1ae2e32a084ca6fab915e1da7c90e428264573d6
Robert Schmidt <rjeschmi@gmail.com>
2015-12-23 20:41:40 -0500

    updating update script

--- scripts/update.py
+++ scripts/update.py
@@ -1,45 +1,55 @@
 #!/usr/bin/env python
-
+"""
+A simple module to update easybuild repository
+"""
 
 import os
+import sys
 from multiprocessing import Pool
 from subprocess import Popen, PIPE
 
-branchexception = { 
+BRANCHEXCEPTION = {
     'vsc-base': 'master',
     'easybuild': 'master',
-    'easybuild-wiki': 'master' }
+    'easybuild-wiki': 'master'
+}
 
 def gitfetch(repo):
-    process = Popen( ["git", "fetch", "--all" ], cwd=repo, stdout=PIPE, stderr=PIPE)
+    """fetch each repo by changing into the directory"""
+    process = Popen(
+        ["git", "fetch", "--all"], cwd=repo, stdout=PIPE, stderr=PIPE
+    )
     output = process.communicate()[0]
     return "fetching %s output: %s" % (repo, output)
 
 def gitmerge(repo):
-    defbranch = branchexception.setdefault(repo, "develop")
-    process = Popen( ["git", "merge", "--ff-only", "origin/"+defbranch ], cwd=repo, stdout=PIPE, stderr=PIPE)
+    defbranch = BRANCHEXCEPTION.setdefault(repo, "develop")
+    process = Popen(
+        ["git", "merge", "--ff-only", "origin/"+defbranch],
+        cwd=repo, stdout=PIPE, stderr=PIPE
+    )
     output = process.communicate()[0]
     return "fetching %s output: %s" % (repo, output)
 
 def gitdefcheck(repo):
-    defbranch = branchexception.setdefault(repo, "develop")
-    process = Popen( ["git", "checkout", defbranch ], cwd=repo, stdout=PIPE, stderr=PIPE)
+    defbranch = BRANCHEXCEPTION.setdefault(repo, "develop")
+    process = Popen(["git", "checkout", defbranch ], cwd=repo, stdout=PIPE, stderr=PIPE)
     output = process.communicate()[0]
     return "merging %s output: %s" % (repo, output)
-   
+
 
 def printoutput(out):
-    for x in out:
-        print x
- 
-if __name__ == '__main__':
+    for line in out:
+        print line
+
+def main():
     print "creating a 4 process pool"
     pool = Pool(processes=4)
-    dirs = filter(os.path.isdir, os.listdir("."))
+    dirs = [repo for repo in os.listdir(".") if os.path.isdir(repo)]
     print "applying gitfetch to %s" % dirs
     printoutput(pool.map(gitdefcheck, dirs))
-    printoutput( pool.map(gitfetch, dirs))
-    printoutput( pool.map(gitmerge, dirs))
-            
+    printoutput(pool.map(gitfetch, dirs))
+    printoutput(pool.map(gitmerge, dirs))
 
-
+if __name__ == '__main__':
+    main()
